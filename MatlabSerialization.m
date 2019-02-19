e8aee19d15954a97b004d0680544b63d0c8ee040
Steve Miller <slmille4@gmail.com>
2019-02-18 19:05:39 -0500

    update matlab code

---
+++ MatlabSerialization.m
@@ -0,0 +1,21 @@
+t = tcpip('0.0.0.0', 54000, 'NetworkRole', 'server');
+t.InputBufferSize = 134217727;
+% %Open a connection. This will not return until a connection is received.
+% 
+fopen(t);
+% %Read the waveform and confirm it visually by plotting it.
+BytesAvailable = 0;
+while BytesAvailable == 0 || BytesAvailable ~= t.BytesAvailable
+    BytesAvailable = t.BytesAvailable;
+    pause(1)
+end
+
+data = fread(t, t.BytesAvailable, 'uint8');
+mat = hlp_deserialize(data)
+%data = fscanf(t);
+
+fclose(t);
+delete(t);
+clear t;
+clear BytesAvailable
+clear data
